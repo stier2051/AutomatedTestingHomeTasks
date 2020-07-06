74f27a277e9077f438d2edcec3df0174b6d871aa
Alexandr Mun <munakaz@yandex.ru>
2020-07-06 17:11:16 +0600

    create maven project

---
+++ Java/src/main/java/com/mun/automated/App.java
@@ -0,0 +1,13 @@
+package com.mun.automated;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
