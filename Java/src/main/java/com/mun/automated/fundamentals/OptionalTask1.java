2ae5ac6158d0a13e3345b7a21be88b50b33f0842
Alexandr Mun <munakaz@yandex.ru>
2020-07-11 18:58:04 +0600

    fundamentals.OptionalTask1

--- Java/src/main/java/com/mun/automated/fundamentals/OptionalTask1.java
+++ Java/src/main/java/com/mun/automated/fundamentals/OptionalTask1.java
@@ -1,4 +1,4 @@
-//18.05.2020
+//11.07.2020
 //Alexandr Mun
 //Найти самое короткое и самое длинное число. Вывести найденные числа и их длину.
 
@@ -10,18 +10,29 @@ import java.io.InputStreamReader;
 
 public class OptionalTask1 {
     public static void main(String[] args) throws IOException {
-
-        int max = 0;
         int[] array = new int[10];
+        int longest = 0;
+        int shortest = 1;
+        int longNumber = 0;
+        int shortNumber = 0;
+
 
         System.out.println("Введите 10 чисел");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         for (int i = 0; i < array.length; i++) {
             array[i] = Integer.parseInt(reader.readLine());
-            if (array[i] > max) {
-                max = array[i];
+            int lengthNumber = String.valueOf(Math.abs(array[i])).length();
+            if (lengthNumber > longest) {
+                longest = lengthNumber;
+                longNumber = array[i];
+            }
+            if (lengthNumber <= shortest) {
+                shortest = lengthNumber;
+                shortNumber = array[i];
             }
         }
+        System.out.println("Самое короткое число: " + shortNumber + ", длина: " + shortest);
+        System.out.println("Самое длинное число: " + longNumber + ", длина: " + longest);
     }
 }
